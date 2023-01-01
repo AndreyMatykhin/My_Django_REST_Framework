6f766e048b8544580d93fcf0945255b200c144b5
MAA <matykhinand2021@gmail.com>
2023-01-01 10:56:57 +0300

    DZ compleate

---
+++ My_TODO/my_TODO/frontend/src/components/MainMenu.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const MainMenu = () => {
+    return(
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#"> MAIN MENU
+                </a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"> <a class="nav-link" href="#"> "Users"</a></li>
+                        <li class="nav-item"> <a class="nav-link" href=""> "Category"</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+export default MainMenu
