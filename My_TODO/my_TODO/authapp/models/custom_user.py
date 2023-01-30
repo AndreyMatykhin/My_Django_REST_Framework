a40a10ab45cf9fd1a7aa13472323becc4780c1cf
AndreyMatykhin <93869906+AndreyMatykhin@users.noreply.github.com>
2023-01-30 07:37:39 +0300

    Matyukhin andrey dz6 (#6)
    * change customuser
    
    * Change customuser
    
    * dz6 compleate

--- My_TODO/my_TODO/authapp/models/custom_user.py
+++ My_TODO/my_TODO/authapp/models/custom_user.py
@@ -1,13 +1,11 @@
 from django.db import models
 from uuid import uuid4
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.contrib.auth.validators import ASCIIUsernameValidator
-from django.core.mail import send_mail
 from django.utils.translation import gettext_lazy as _
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
+class CustomUser(AbstractUser):
     username_validator = ASCIIUsernameValidator()
 
     class UserCategory(models.TextChoices):
@@ -16,57 +14,38 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         DEVELOPERS = 'DV', _('Developers')
 
     id = models.UUIDField(default=uuid4, primary_key=True)
+    username = models.CharField(_("username"),
+                                max_length=150,
+                                unique=True,
+                                help_text=_("Required. 150 characters or fewer. ASCII letters and digits only."),
+                                validators=[username_validator],
+                                error_messages={"unique": _("A user with that username already exists."), },
+                                )
+    email = models.CharField(_("email address"),
+                             max_length=256,
+                             unique=True,
+                             error_messages={"unique": _("A user with that email address already exists."), },
+                             )
+    user_category = models.CharField(_("user category"),
+                                     max_length=2,
+                                     choices=UserCategory.choices,
+                                     default=UserCategory.DEVELOPERS,
+                                     help_text=_("Defines the category of the project user."),
+                                     )
+    is_staff = models.BooleanField(_("staff status"),
+                                   default=False,
+                                   help_text=_("Designates whether the user can log into this admin site."),
+                                   )
+    is_active = models.BooleanField(_("active"),
+                                    default=True,
+                                    help_text=_("Designates whether this user should be treated as active. "
+                                                "Unselect this instead of deleting accounts."),
+                                    )
+    date_joined = models.DateTimeField(_("date joined"), auto_now_add=True)
 
-    username = models.CharField(
-        _("username"),
-        max_length=150,
-        unique=True,
-        help_text=_("Required. 150 characters or fewer. ASCII letters and digits only."),
-        validators=[username_validator],
-        error_messages={
-            "unique": _("A user with that username already exists."),
-        },
-    )
-    first_name = models.CharField(_("first name"), max_length=150, blank=True)
-    last_name = models.CharField(_("last name"), max_length=150, blank=True)
-    email = models.CharField(
-        _("email address"),
-        max_length=256,
-        unique=True,
-        error_messages={
-            "unique": _("A user with that email address already exists."),
-        },
-    )
-    user_category = models.CharField(
-        _("user category"),
-        max_length=2,
-        choices=UserCategory.choices,
-        default=UserCategory.DEVELOPERS,
-        help_text=_("Defines the category of the project user."),
-    )
-    is_staff = models.BooleanField(
-        _("staff status"),
-        default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
-    )
+    objects = UserManager()
 
     def is_upperclass(self):
         return self.user_category in {self.UserCategory.ADMINISTRATOR, self.UserCategory.MANAGER, }
 
-    is_active = models.BooleanField(
-        _("active"),
-        default=True,
-        help_text=_(
-            "Designates whether this user should be treated as active. \
-            Unselect this instead of deleting accounts."
-        ),
-    )
-    date_joined = models.DateTimeField(_("date joined"), auto_now_add=True)
-
-    objects = UserManager()
-    EMAIL_FIELD = "email"
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["email"]
-
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        send_mail(subject, message, from_email, [self.email], **kwargs)
+    # def _do_update(self, base_qs, using, pk_val, values, update_fields, forced_update):
--- My_TODO/my_TODO/authapp/serializers/user_model.py
+++ My_TODO/my_TODO/authapp/serializers/user_model.py
@@ -7,16 +7,33 @@ class CustomUserModelSerializer(HyperlinkedModelSerializer):
         model = CustomUser
         fields = ['url',
                   'username',
-                  'first_name',
-                  'last_name',
                   'email',
                   'user_category',
                   'is_active',
                   'date_joined',
+                  'password'
                   ]
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        super().update(instance, validated_data)
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
 
 
 class CustomUserSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        super().update(instance, validated_data)
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
