5c1816e02b02ea3012f4ef4478a1cdc15ee31c16
MAA <matykhinand2021@gmail.com>
2023-01-31 07:50:34 +0300

    create LoginForm

--- My_TODO/my_TODO/frontend/src/App.js
+++ My_TODO/my_TODO/frontend/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-//import logo from './logo.svg';
-//import './App.css';
 import UserList from './components/Users.js'
 import ProjectsList from './components/Projects.js'
 import TODOsList from './components/TODO.js'
 import TODOsProjectList from './components/TODOsProject.js'
-
-import Footer from './components/Footer.js'
-import MainMenu from './components/MainMenu.js'
-
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import LoginForm from './components/Auth.js'
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
 import axios from 'axios'
 
 const NotFound404 = ({ location }) => {
@@ -29,7 +24,8 @@ class App extends React.Component {
             'TODOs':[]
         }
     }
-    componentDidMount() {
+
+    load_data(){
         axios.get('http://127.0.0.1:8000/authapp').then(response => {
                     this.setState({'users': response.data})
                     }).catch(error => console.log(error))
@@ -40,21 +36,44 @@ class App extends React.Component {
                     this.setState({'TODOs': response.data})
                     }).catch(error => console.log(error))
     }
+    componentDidMount() {
+        this.load_data()
+    }
 
     render () {
         return (
             <div className='App'>
                 <BrowserRouter>
-                    <MainMenu />
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item"> <Link to='/users'>List of Users</Link></li>
+                            <li class="nav-item"> <Link to='/projects'>List of Projects</Link></li>
+                            <li class="nav-item"> <Link to='/TODO'>list of ToDo</Link></li>
+                        </ul>
+                    </div>
                     <Switch>
                         <Route exact path='/users' component={()=> <UserList users={this.state.users} /> }/>
                         <Route exact path='/projects' component={()=> <ProjectsList projects={this.state.projects} /> }/>
                         <Route exact path='/TODO' component={()=> <TODOsList TODOs={this.state.TODOs} /> }/>
                         <Route path='/TODO/:project' component={()=> <TODOsProjectList TODOs={this.state.TODOs} /> }/>
                         <Route exact path='/' component={()=>(<h1>Выберите пункт Меню</h1>)}/>
+                        <Route exact path='/login' component={() => <LoginForm />} />
                         <Route component={NotFound404} />
                     </Switch>
-                <Footer />
+                    <div class="col-sm-6 col-md-3 text-center">
+                        <p><strong>My TODO</strong></p>
+                        <p>
+                            <ul class="list-unstyled">
+                                <li><Link to='/'>Домашняя</Link></li>
+                                <li><Link to='/login'>Login</Link></li>
+                            </ul>
+                        </p>
+                        <p><strong>Мы в социальных сетях</strong></p>
+                        <p><strong>Наше приложение</strong></p>
+                        <div>
+                            <p><small>&copy; My TODO 2023</small></p>
+                        </div>
+                    </div>
                 </BrowserRouter>
             </div>
             );
